699de2b8200c570f0a96c5ad07351a694e180101
rick <oops11234@gmail.com>
2021-10-18 23:34:52 +0800

    add day&night mode

--- havana.js
+++ havana.js
@@ -1,3 +1,4 @@
+// slider
 $(document).ready(function(){
       $('.owl-carousel').owlCarousel({
             loop:true,
@@ -18,27 +19,72 @@ $(document).ready(function(){
         })
     });
 
-
+// function Button
 const openBtn = document.querySelector('.openBtn')
 const sideMenu = document.querySelector('.side_menu')
       openBtn.addEventListener("click" ,function(){
           if(openBtn.className === "openBtn open"){
-            //    openBtn.innerHTML=''
-            //     openBtn.innerHTML=`<i class="close_btn neon-effectTxt_pink fas fa-times">`
-            //     sideMenu.className+=` side_menu_show`
             openBtn.innerHTML=''
-            openBtn.innerHTML=`<i class="fas fa-bars neon-effectTxt_blue">`
             sideMenu.classList.remove('side_menu_show')
             openBtn.classList.remove('open')
+            if(switchBtn.className === "switchBtn day"){
+               openBtn.innerHTML=`<i class="fas fa-bars neon-effectTxt_blue"> `
+            }else{
+              openBtn.innerHTML=`<i class="fas fa-bars neon-effectTxt_blue day_modelBlue"> `
+            }
           }
-          else{
-            openBtn.classList.add('open')
+          else{ 
+            openBtn.classList.add('open') 
             openBtn.innerHTML=''
-            openBtn.innerHTML=`<i class="close_btn neon-effectTxt_pink fas fa-times">`
+            openBtn.innerHTML=`<i class="fas fa-times">`
             sideMenu.className+=` side_menu_show`
+            if(switchBtn.className === "switchBtn day"){
+              openBtn.innerHTML=`<i class="neon-effectTxt_pink fas fa-times">`
+            }
           }
       })
-      const loadPage = document.querySelector('.loadPage')
+
+const switchBtn = document.querySelector(".switchBtn")
+      switchBtn.addEventListener('click',function(){
+        if(switchBtn.className === "switchBtn day"){
+          switchBtn.innerHTML=""
+          switchBtn.innerHTML=`<i class="fas fa-moon neon-effectTxt_dayIcon"></i>`
+          switchBtn.classList.remove('day')
+        }else{
+          switchBtn.classList.add('day')
+          switchBtn.innerHTML=""
+          switchBtn.innerHTML=`<i class="fas fa-sun"></i>`
+        }
+      })
+
+const clickBtn= document.querySelector('.switchBtn')
+      clickBtn.addEventListener('click', nightMode)
+const dayBanner=document.querySelectorAll(".day_banner")
+const pinkBtn = document.querySelectorAll(".pinkBlackShine_btn")
+
+function nightMode(){
+  const titleNeon=document.querySelectorAll(".neon-effectTxt_blue")
+      document.querySelectorAll('.backImg').forEach(element => {
+          element.classList.toggle("day_banner")
+      })
+      titleNeon.forEach(element => {
+        element.classList.toggle("day_modelBlue")
+      })
+      pinkBtn.forEach(element => {
+        element.classList.toggle("pinkBlackShine_btn")
+      }
+      )}
+
+
+// loadPage
+const loadPage = document.querySelector('.loadPage')
       window.setTimeout(()=>{
         loadPage.classList.add(`loadPageOff`)
-      },5000)
+      },500)
+
+
+
+
+
+
+
