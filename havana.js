0a54fe159184f92e67b6e51818aff2b2942a2d91
rick <oops11234@gmail.com>
2021-10-29 00:11:18 +0800

    homepage 414px aboutUs adj TBD

--- havana.js
+++ havana.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
                 0:{
                     items:1
                 },
-                600:{
+                768:{
                     items:1
                 },
                 1200:{
@@ -29,11 +29,11 @@ $(document).ready(function(){
             0:{
                 items:1
             },
-            600:{
+            768:{
                 items:3
             },
             1200:{
-                items:3
+                items:5
             }
         }
     })
